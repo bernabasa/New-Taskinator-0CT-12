27ae077277560d03025c8f4ab58c2e2745d2f39e
bernabasa <75853223+bernabasa@users.noreply.github.com>
2022-10-20 16:01:32 -0400

    add ability to update and delete tasks

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,77 @@
+
+var taskInProgressE1 = document.querySelector("#tasks-in-progress");
+var tasksCompletedE1 = document.querySelector("#tasks-completed");
+
+var pageContentEl = document.querySelector("#page-content");
+
+var taskButtonHandler = function(event) {
+  console.log(event.target);
+
+  if (event.target.matches(".delete-btn")) {
+    // get the element's task id
+    var taskId = event.target.getAttribute("data-task-id");
+    deleteTask(taskId);
+  }
+  // get target element from event
+  var targetE1 = event.target;
+
+  // edit button was clicked
+
+  if (targetE1.matches(".edit-btn")) {
+    var taskId = targetE1.getAttribute("data-task-id");
+    editTask(taskId);
+  }
+
+  // delete button was clicked
+
+  else if (targetE1.matches(".delete-btn")) {
+    var taskId = targetE1.getAttribute("data-task-id");
+    deleteTask(taskId);
+  }
+};
+var taskStatusChangeHandler = function(event) {
+  // get the task item's id
+  var taskId = event.target.getAttribute("data-task-id");
+
+  // get the currently selected option's value and convert to lowercase
+  var statusValue = event.target.value.toLowerCase();
+
+  // find the parent task item element based on the id
+  var taskSelected = document.querySelector(".task-item[data-task-id'" + taskId + "']");
+
+  if (statusValue === "to do") {
+    tasksToDoEl.appendChild(taskSelected);
+  }
+  else if (statusValue === "in progress") {
+    tasksInProgressE1.appendChild(taskSelected);
+  }
+  else if (statusValue === "completed") {
+    tasksCompletedE1.appendChild(taskSelected);
+  }
+};
+
+var editTask = function(taskId) {
+  console.log("editing task #" + taskId);
+  // get task list item element
+  var taskSelected = document.querySelector(".task-item[data-task-id='" + taskId + "']");
+// get content from task name and type
+var taskName = taskSelected.querySelector("h3.task-name").textContent;
+var taskType = taskSelected.querySelector("span.task-type").textContent;
+document.querySelector("input[name='task-name']").value = taskName;
+document.querySelector("select[name='task-type']").value = taskType;
+document.querySelector("#save-task").textContent = "Save Task";
+
+formEl.setAttribute("data-task-id", taskId);
+};
+
+pageContentEl.addEventListener("click", taskButtonHandler);
+
+var deleteTask = function(taskId) {
+  var taskSelected = document.querySelector(".task-item[data-task-id='" + taskId + "']");
+  taskSelected.remove();
+};
+
+
 var taskIdCounter = 0;
 
 var formEl = document.querySelector("#task-form");
@@ -20,12 +94,37 @@ var taskFormHandler = function(event) {
   document.querySelector("input[name='task-name']").value = "";
   document.querySelector("select[name='task-type']").selectedIndex = 0;
 
+  var isEdit = formEl.hasAttribute("data-task-id");
+  createTaskEl(taskDataObj);
+  
+  if(isEdit) {
+    var taskId = formEl.getAttribute("data-task-id");
+    completedEditTask(taskNameInput, taskTypeInput, taskId);
+
+  }
+
+  // no data attribute, so create object as normal and pass to createTaskE1 function
+  else {
   var taskDataObj = {
     name: taskNameInput,
     type: taskTypeInput
   };
 
   createTaskEl(taskDataObj);
+}
+};
+
+var completedEditTask = function(taskName, taskType, taskId) {
+ // find the matching task list item
+ var taskSelected = document.querySelector(".task-item[data-task-id='" + taskId + "']");
+
+ // set new values
+ taskSelected.querySelector("h3.task-name").textContent = taskName;
+taskSelected.querySelector("span.task-type").textContent = taskType;
+
+alert("Task Updated!");
+formEl.removeAttribute("data-task-id");
+document.querySelector("#save-task").textContent = "Add Task";
 };
 
 var createTaskEl = function(taskDataObj) {
@@ -97,3 +196,4 @@ var createTaskActions = function(taskId) {
 }
 
 formEl.addEventListener("submit", taskFormHandler);
+pageContentEl.addEventListener("change", taskStatusChangeHandler);
