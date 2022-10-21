06675d80cc6182f6c1ef0a5f169d0fd58893fc68
bernabasa <75853223+bernabasa@users.noreply.github.com>
2022-10-21 12:46:56 -0400

    add data persistence

--- assets/js/script.js
+++ assets/js/script.js
@@ -220,7 +220,21 @@ var deleteTask = function(taskId) {
 var saveTasks = function() {
   localStorage.setItem("tasks", JSON.stringify(tasks));
 }
+ 
+var loadTasks = function() {
+ var savedTasks = localStorage.getItem("tasks");
 
+ if(!savedTasks) {
+  return false;
+ }
+ savedTasks = JSON.parse(savedTasks);
+
+// loop through savedTasks array
+for (var i = 0; i < savedTasks.length; i++) {
+  // pass each task object into the createTaskE1() function
+  createTaskEl(savedTasks[i]);
+}
+}
 // Create a new task
 formEl.addEventListener("submit", taskFormHandler);
 
@@ -229,3 +243,5 @@ pageContentEl.addEventListener("click", taskButtonHandler);
 
 // for changing the status
 pageContentEl.addEventListener("change", taskStatusChangeHandler);
+
+loadTasks();
